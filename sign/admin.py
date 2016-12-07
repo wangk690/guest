457c94007f5031b8db8a9b7d683ebdfb7db80d87
andrew.wang <wangk690@163.com>
2016-12-08 00:05:13 +0800

    添加models,后台能增加项目和嘉宾

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from sign.models import Event,Guest
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+	list_display = ['name','status','start_time','id']
+	search_fields = ['name'] #搜索栏
+	list_filter = ['status'] #过滤器
+
+class GuestAdmin(admin.ModelAdmin):
+	"""docstring for GuestAdmin"""
+	list_display = ['realname','phone','email','sign','creat_time','event']
+	search_fields = ['realname','phone'] #搜索栏
+	list_filter = ['sign'] #过滤器		
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(Guest,GuestAdmin)
