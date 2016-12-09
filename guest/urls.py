fe9187452fdebd873aeee732f2aace932bb5e713
wangk690 <wangk690@163.com>
2016-12-09 15:16:21 +0800

    add Event&Guest pages

--- guest/urls.py
+++ guest/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     url(r'^index/$', views.index),
     url(r'^login_action/$', views.login_action),
     url(r'^event_manage/$', views.event_manage),
+    url(r'^guest_manage/$', views.guest_manage),
     url(r'^accounts/login/$', views.index),
+    url(r'^search_name/$', views.search_name),
 ]
--- sign/views.py
+++ sign/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from sign.models import Event,Guest
 
 # Create your views here.
 def index(request):
@@ -40,5 +41,22 @@ def login_action(request):
 @login_required
 def event_manage(request):
 	#username = request.COOKIES.get('user','') #读取浏览器cookie
+	event_list = Event.objects.all()
 	username = request.session.get('user','')
-	return render(request, 'event_manage.html',{'user':username})
+	return render(request, 'event_manage.html',{'user':username,'events':event_list})
+
+#发布会名称凑数
+@login_required
+def search_name(request):
+	username = request.session.get('user','')
+	search_name = request.GET.get('name','')
+	event_list = Event.objects.filter(name__contains=search_name)
+	return render(request, 'event_manage.html',{'user':username,'events':event_list})
+
+# 嘉宾管理
+@login_required
+def guest_manage(request):
+	#username = request.COOKIES.get('user','') #读取浏览器cookie
+	guest_list = Guest.objects.all()
+	username = request.session.get('user','')
+	return render(request, 'guest_manage.html',{'user':username,'guests':guest_list})
