299fc3b5c6db8ba06299b080976d64bf4f5aafd4
wangk690 <wangk690@163.com>
2016-12-14 18:34:17 +0800

    移动count.py，增加Django test

--- sign/tests.py
+++ sign/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from sign.models import Guest,Event
 
 # Create your tests here.
+class ModelTest(TestCase):
+	"""docstring for ModelTest"""
+	
+	def setUp(self):
+		Event.objects.create(id=7,name='oneplus 4 event',status=True,limit=2000,address='shenzhen',start_time='2016-08-31 02:18:31')
+		Guest.objects.create(id=7,event_id=7,realname='alena',phone='13712341235',email='alena@mail.com',sign=False)
+
+	def test_event_model(self):
+		result = Event.objects.get(name='oneplus 4 event')
+		self.assertEqual(result.address, 'shenzhen')
+		self.assertTrue(result.status)
+
+	def test_guest_model(self):
+		result = Guest.objects.get(phone='13712341235')
+		self.assertEqual(result.realname, 'alena')
+		self.assertFalse(result.sign)
+		
